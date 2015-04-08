db0b08f10651a41f9d9ea9936c07326f4695e82d
Michael <michael@doublespeakgames.com>
2015-04-07 23:51:00 -0400

    fully dynamic angle wipe. awesome.

--- script.js
+++ script.js
@@ -2,11 +2,32 @@
 	
 	function _initElement(element) {
 		var content = element.innerHTML
-		, borderEl = document.createElement('div')
+		, borderCont = document.createElement('div')
+		, border = document.createElement('div')
+		, elWidth = element.offsetWidth
+		, elHeight = element.offsetHeight
+		, diag = Math.ceil(Math.sqrt((elWidth * elWidth) + (elHeight * elHeight)))
 		;
 
-		borderEl.className = 'border-container';
-		element.insertBefore(borderEl, element.firstChild);
+
+		borderCont.className = 'border-container';
+		borderCont.style.width = diag + 'px'; borderCont.style.height = diag + 'px';
+		borderCont.style.top = (elHeight - diag) / 2 + 'px';
+		borderCont.style.left = (elWidth - diag) / 2 + 'px';
+		borderCont.style.transform = 'rotate(45deg) translate3d(-' + diag + 'px, 0, 0)';
+		border.className = 'border';
+		border.style.width = elWidth + 'px'; border.style.height = elHeight + 'px';
+		border.style.top = (diag - elHeight) / 2 + 'px';
+		border.style.left = (diag - elWidth) / 2 + 'px';
+		border.style.transform = 'translate3d(' + diag + 'px, 0, 0) rotate(-45deg)';
+
+		borderCont.appendChild(border);
+		element.insertBefore(borderCont, element.firstChild);
+
+		borderCont.style.overflow = 'visible';
+		setTimeout(function() {
+			borderCont.style.overflow = 'hidden';
+		}, 0);
 	}
 
 	window.addEventListener('load', function() {
